3d21aeed33a402e9fbeccb790e8bbacdd2565f08
Sky <156252108@qq.com>
2022-10-25 09:21:11 +0800

    第一个java程序

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("第一个java程序，实现本地创建项目，和github互通!");
+
     }
 }
